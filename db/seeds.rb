fcb34e983b7b051fa00188016222076242985421
Diego Alcocer <diegoaalcocer@gmail.com>
2017-11-17 22:00:04 +0000

    Implemented the ability to show statuses on the admin dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-@user = User.create(email: "diegoaalcocer@gmail.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Diego", last_name: "A")
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Diego", last_name: "A")
 
 puts "1 User created"
+
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+
+puts "1 Admin User created"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
