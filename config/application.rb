c4d999ec0c2d485f9b0f124dc4e4cdd6cb3ae47f
Diego Alcocer <diegoaalcocer@gmail.com>
2017-11-27 21:15:34 +0000

    Added phone attribute to User and made it required

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -6,7 +6,6 @@ Bundler.require(*Rails.groups)
 
 module Overtime
   class Application < Rails::Application
-    config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths << Rails.root.join("lib")
   end
 end
